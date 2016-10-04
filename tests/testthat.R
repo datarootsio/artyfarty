053a6bb59fba63b54c89494e08ce84c22ddbe604
Bart6114 <bartsmeets86@gmail.com>
2016-10-04 17:59:22 +0200

    basic testing

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(artyfarty)
+
+test_check("artyfarty")
---
+++ tests/testthat/test_valid_obj.R
@@ -0,0 +1,18 @@
+library(artyfarty)
+
+context("themes return correctly")
+
+test_that("themes return a theme object", {
+
+  theme_funcs<-Filter(function(o) grepl("theme_", o), ls("package:artyfarty"))
+
+  for(th in theme_funcs){
+
+    expect_s3_class(get(th)(), "theme")
+  }
+
+
+
+
+
+})
